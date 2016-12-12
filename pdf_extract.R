501e90321108904bd1365865e84284d8f012b8f5
vpascal <vpascal2@gmail.com>
2016-12-12 15:25:09 -0500

    extract numeric

--- pdf_extract.R
+++ pdf_extract.R
@@ -1,16 +1,18 @@
 library(pdftools)
 library(stringr)
 library(dplyr)
-
+library(purrr)
 
 file <- "https://www.ohio.edu/instres/compendium/profile/EDUProfile.pdf"
 
 txt <- pdf_text(file)
 pages2 <- txt[2]
-
+pages2 <- gsub(pattern = "\\---","0" ,pages2)
 temp <- unlist(strsplit(pages2, "\\r\\n"))
 
-str_extract_all(pattern = "\\d{4}", temp)
-str_extract_all(pattern = "\\d{4}", temp)
+temp <- str_extract_all(pattern = "\\d{1,}[\\,\\.]\\d{1,}|\\d{1,} | \\d{2}\\-\\d{2}", temp)
 
-str_extract_all(pattern = "\\d{1,}[\\,\\.]\\d{1,}|\\d{1,} | \\d{2}\\-\\d{2}", pages2)
+
+
+do.call(rbind,temp)
+
